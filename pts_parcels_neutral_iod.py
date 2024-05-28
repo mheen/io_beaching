ead25b0c625e1b5a8e91abb39e21aed74272727f
Mirjam van der Mheen <mirjam.vandermheen@uwa.edu.au>
2024-05-28 15:52:40 +0800

    Added simulations with surface currents and wind with Indonesian sources

--- pts_parcels_neutral_iod.py
+++ pts_parcels_neutral_iod.py
@@ -33,6 +33,29 @@ def run_neutral_iod(output_name='neutral_iod_2008-2009',constant_release=False):
     run_hycom_subset_monthly_release(input_dir,output_dir,output_name,
                                      io_sources.time0,io_sources.lon0,io_sources.lat0,
                                      start_date,end_date)
+
+def run_neutral_iod_with_wind_and_indonesian_sources(output_name='neutral_iod_2008-2009_3pwindage_indonesian-sources.nc',
+                                                     constant_release=False, windage=0.03):
+    output_dir = get_dir('pts_output')
+    start_date = datetime(2008, 1, 1, 12, 0)
+    end_date = datetime(2009, 12, 31)
+    io_sources = IORiverParticles.get_from_netcdf(start_date, constant_release=constant_release,
+                                                  ocean_basin='io_indo')
+    run_hycom_cfsr_subset_monthly_release(output_dir, output_name,
+                                          io_sources.time0, io_sources.lon0, io_sources.lat0,
+                                          start_date, end_date, windage=windage)
+
+def run_neutral_iod_with_indonesian_sources(output_name='neutral_iod_2008-2009_indonesian-sources.nc',
+                                            constant_release=False):
+    input_dir = get_dir('hycom_input')
+    output_dir = get_dir('pts_output')
+    start_date = datetime(2008, 1, 1, 12, 0)
+    end_date = datetime(2009, 12, 31)
+    io_sources = IORiverParticles.get_from_netcdf(start_date, constant_release=constant_release,
+                                                  ocean_basin='io_indo')
+    run_hycom_subset_monthly_release(input_dir, output_dir, output_name,
+                                     io_sources.time0, io_sources.lon0, io_sources.lat0,
+                                     start_date, end_date)
     
 def run_neutral_iod_with_stokes_and_indonesian_sources(output_name='neutral_iod_2008-2009_stokes_indonesian-sources.nc',
                                                        constant_release=False):
@@ -49,4 +72,6 @@ if __name__ == '__main__':
     # output_name = 'neutral_iod_constant_sources_2008-2009'
     # run_neutral_iod(output_name=output_name,constant_release=True)
     # run_neutral_iod_2009_with_wind()
-    run_neutral_iod_with_stokes_and_indonesian_sources()
+    # run_neutral_iod_with_stokes_and_indonesian_sources()
+    # run_neutral_iod_with_indonesian_sources()
+    run_neutral_iod_with_wind_and_indonesian_sources()
